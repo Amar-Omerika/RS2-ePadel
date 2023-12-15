f4d3ab1b151318fae84cb3e7f3cfad0f0f035e02
Amar-Omerika <amar.omerika@edu.fit.ba>
2023-12-15 14:45:44 +0100

    Added validation for korisnik insert and  updated request

--- ePadel/ePadel.Model/Requests/KorisnikRequest/KorisnikInsertRequest.cs
+++ ePadel/ePadel.Model/Requests/KorisnikRequest/KorisnikInsertRequest.cs
@@ -1,22 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ePadel.Model.Requests.KorisnikRequest
 {
     public class KorisnikInsertRequest
     {
+        [Required(AllowEmptyStrings = false)]
         public string? Ime { get; set; }
+
+
+        [Required(AllowEmptyStrings = false)]
         public string? Prezime { get; set; }
+
+
+        [Required(AllowEmptyStrings = false)]
+        [MinLength(4, ErrorMessage = "Korisničko ime mora sadržavati najmanje 4 karaktera!")]
         public string? KorisnickoIme { get; set; }
+
+
+        [Required(AllowEmptyStrings = false)]
+        [EmailAddress()]
         public string? Email { get; set; }
+
+
+        [Required(AllowEmptyStrings = false)]
+        [RegularExpression("^\\d{3}-\\d{3}-(\\d{4}|\\d{3})$", ErrorMessage = "Broj telefona mora biti u obliku 06X-XXX-XXX !")]
         public string? BrTelefona { get; set; }
-        public bool? Aktivan { get; set; }
-        public  string? Password { get; set; }
+
+        [Required]
+        [MinLength(8, ErrorMessage = "Lozinka mora sadržavati najmanje 8 karaktera!")]
+        public string? Password { get; set; }
+
         public string? PasswordPotvrda { get; set; }
         public string? Slika { get; set; }
         public List<int>? Uloge { get; set; } = new List<int>();
+        public bool? Aktivan { get; set; }
     }
 }
--- ePadel/ePadel.Model/Requests/KorisnikRequest/KorisnikUpdateRequest.cs
+++ ePadel/ePadel.Model/Requests/KorisnikRequest/KorisnikUpdateRequest.cs
@@ -1,18 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ePadel.Model.Requests.KorisnikRequest
 {
     public class KorisnikUpdateRequest
     {
+        [Required(AllowEmptyStrings = false)]
         public string? Ime { get; set; }
+
+
+        [Required(AllowEmptyStrings = false)]
         public string? Prezime { get; set; }
+
+
+        [Required(AllowEmptyStrings = false)]
+        [EmailAddress()]
         public string? Email { get; set; }
+
+
+        [Required(AllowEmptyStrings = false)]
+        [RegularExpression("^\\d{3}-\\d{3}-(\\d{4}|\\d{3})$")]
         public string? BrTelefona { get; set; }
-        public bool? Aktivan { get; set; }
+
+
+        [Required(AllowEmptyStrings = false)]
+        [MinLength(4)]
         public string? KorisnickoIme { get; set; }
+
+        public bool? Aktivan { get; set; }
     }
 }
