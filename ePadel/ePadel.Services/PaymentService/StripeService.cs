3d4b1833e523da9e95cd1182e8598812c14d61c0
Amar-Omerika <amar.omerika@edu.fit.ba>
2023-12-24 21:02:08 +0100

    Created payment service and added enviroment variable

---
+++ ePadel/ePadel.Services/PaymentService/StripeService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Configuration;
+using Stripe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ePadel.Services.PaymentService
+{
+    public class StripeService
+    {
+        private readonly string apiKey;
+        public StripeService(IConfiguration configuration)
+        {
+            apiKey = configuration["StripeSettings:ApiKey"] ?? Environment.GetEnvironmentVariable("STRIPE_API_KEY");
+        }
+        public string PlatiTermin(int iznos, string opis)
+        {
+            StripeConfiguration.ApiKey = apiKey;
+            var options = new PaymentIntentCreateOptions
+            {
+                Amount = iznos * 100,
+                Currency = "EUR",
+                Description = opis,
+            };
+            var service = new PaymentIntentService();
+            var paymentIntent = service.Create(options);
+
+            return paymentIntent.ClientSecret;
+        }
+    }
+}
