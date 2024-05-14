cd1ab6029df849d1a934ec4f021cff1b17ec05b9
Amar-Omerika <amar.omerika@edu.fit.ba>
2024-05-14 23:16:05 +0200

    Rearranged positioning on add modal

--- epadel_admin/lib/widgets/modals/Tereni/add_teren_modal.dart
+++ epadel_admin/lib/widgets/modals/Tereni/add_teren_modal.dart
@@ -16,8 +16,10 @@ class AddTerenModal extends StatefulWidget {
 class _AddTerenModalState extends State<AddTerenModal> {
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
   String? naziv;
+  String? lokacija;
   int? brojTerena;
   int? cijena;
+  int? popust;
   int? tipTerenaId;
 
   @override
@@ -29,75 +31,123 @@ class _AddTerenModalState extends State<AddTerenModal> {
         child: Column(
           mainAxisSize: MainAxisSize.min,
           children: [
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Naziv Terena',
-              ),
-              onChanged: (value) {
-                naziv = value;
-              },
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Ovo polje je obavezno';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              decoration: const InputDecoration(
-                labelText: 'Broj Terena',
-              ),
-              keyboardType: TextInputType.number,
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.digitsOnly
+            Row(
+              children: [
+                Expanded(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Naziv Terena',
+                        ),
+                        onChanged: (value) {
+                          naziv = value;
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Ovo polje je obavezno';
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Vrsta Podloge',
+                        ),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        onChanged: (value) {
+                          tipTerenaId = int.tryParse(value);
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Ovo polje je obavezno';
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        maxLength: 3,
+                        decoration: const InputDecoration(
+                          labelText: 'Cijena',
+                        ),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        onChanged: (value) {
+                          cijena = int.tryParse(value);
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Ovo polje je obavezno';
+                          }
+                          return null;
+                        },
+                      ),
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Broj Terena',
+                        ),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        onChanged: (value) {
+                          brojTerena = int.tryParse(value);
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Ovo polje je obavezno';
+                          }
+                          return null;
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(width: 16), // Add space between the columns
+                Expanded(
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Lokacija',
+                        ),
+                        onChanged: (value) {
+                          lokacija = value;
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Ovo polje je obavezno';
+                          }
+                          return null;
+                        },
+                      ),  
+                      TextFormField(
+                        decoration: const InputDecoration(
+                          labelText: 'Popust',
+                        ),
+                        keyboardType: TextInputType.number,
+                        inputFormatters: <TextInputFormatter>[
+                          FilteringTextInputFormatter.digitsOnly
+                        ],
+                        onChanged: (value) {
+                          popust = int.tryParse(value);
+                        },
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Unijeli ste da zelite popust, morate unijeti iznos popusta';
+                          }
+                          return null;
+                        },
+                      )
+                    ],
+                  ),
+                ),
               ],
-              onChanged: (value) {
-                brojTerena = int.tryParse(value);
-              },
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Ovo polje je obavezno';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              maxLength: 3,
-              decoration: const InputDecoration(
-                labelText: 'Cijena',
-              ),
-              keyboardType: TextInputType.number,
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.digitsOnly
-              ],
-              onChanged: (value) {
-                cijena = int.tryParse(value);
-              },
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Ovo polje je obavezno';
-                }
-                return null;
-              },
-            ),
-            TextFormField(
-              maxLength: 3,
-              decoration: const InputDecoration(
-                labelText: 'Tip Terena',
-              ),
-              keyboardType: TextInputType.number,
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.digitsOnly
-              ],
-              onChanged: (value) {
-                tipTerenaId = int.tryParse(value);
-              },
-              validator: (value) {
-                if (value == null || value.isEmpty) {
-                  return 'Ovo polje je obavezno';
-                }
-                return null;
-              },
             ),
           ],
         ),
@@ -112,7 +162,7 @@ class _AddTerenModalState extends State<AddTerenModal> {
         ElevatedButton(
           onPressed: () {
             if (formKey.currentState!.validate()) {
-              widget.handleAdd(naziv!, cijena!, brojTerena!, tipTerenaId!);
+              widget.handleAdd(naziv, cijena, brojTerena, tipTerenaId);
             }
           },
           child: const Text('Dodaj'),
