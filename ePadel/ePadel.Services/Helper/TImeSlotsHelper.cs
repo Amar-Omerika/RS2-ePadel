5c33dda21aabb08457fdae9f8dbfb6209b150de8
Amar-Omerika <amar.omerika@edu.fit.ba>
2024-05-22 21:22:07 +0200

    Added time slots helper function

---
+++ ePadel/ePadel.Services/Helper/TImeSlotsHelper.cs
@@ -0,0 +1,26 @@
+﻿namespace ePadel.Services.Helper
+{
+    public static class TImeSlotsHelper
+    {
+        public static List<string> getTimeSlots()
+        {
+            List<string> timeSlots = new List<string>()
+        {
+            "08:00-09:00",
+            "09:00-10:00",
+            "10:00-11:00",
+            "11:00-12:00",
+            "12:00-13:00",
+            "13:00-14:00",
+            "14:00-15:00",
+            "15:00-16:00",
+            "16:00-17:00",
+            "17:00-18:00",
+            "18:00-19:00",
+            "19:00-20:00",
+            "20:00-21:00"
+        };
+            return timeSlots;
+        }
+    }
+}
