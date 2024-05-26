b3139e6ae7c7897b3432a8267c27736e8cd8108f
Amar-Omerika <amar.omerika@edu.fit.ba>
2024-05-26 20:19:55 +0200

    Button positioned successfuly on screen

--- epadel_admin/lib/screens/rezervacije_screen.dart
+++ epadel_admin/lib/screens/rezervacije_screen.dart
@@ -99,102 +99,105 @@ class _RezervacijeScreenState extends State<RezervacijeScreen> {
                           color: Colors.grey[200],
                           borderRadius: BorderRadius.circular(10.0),
                         ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 20),
+                  Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          color: const Color(0xFFD7D2DC),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        padding: const EdgeInsets.all(16.0),
                         child: Row(
                           children: [
-                            Text('Filter: '),
-                            PopupMenuButton<String>(
-                              icon: Icon(Icons.filter_list),
-                              onSelected: (String result) {
-                                setState(() {
-                                  _selectedFilter = result;
-                                  // Implement filter logic
-                                  loadData(); // Call loadData() after setting the filter
-                                });
-                              },
-                              itemBuilder: (BuildContext context) =>
-                                  <PopupMenuEntry<String>>[
-                                const PopupMenuItem<String>(
-                                  value: 'Svi',
-                                  child: Text('Svi'),
-                                ),
-                                const PopupMenuItem<String>(
-                                  value: 'Padel teren 1',
-                                  child: Text('Padel teren 1'),
-                                ),
-                                const PopupMenuItem<String>(
-                                  value: 'Padel teren 2',
-                                  child: Text('Padel teren 2'),
-                                ),
-                                const PopupMenuItem<String>(
-                                  value: 'Padel teren 3',
-                                  child: Text('Padel teren 3'),
-                                ),
-                              ],
+                            Expanded(
+                              child: Column(
+                                children: [
+                                  Text('Ukupan broj rezervacija',
+                                      style: TextStyle(fontSize: 16)),
+                                  Container(
+                                    width: 300,
+                                    height: 100,
+                                    padding: const EdgeInsets.all(8.0),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    child: Text(
+                                      '320',
+                                      style: TextStyle(
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.bold),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            Expanded(
+                              child: Column(
+                                children: [
+                                  Text('Broj iznajmljenih reketa',
+                                      style: TextStyle(fontSize: 16)),
+                                  Container(
+                                    width: 300,
+                                    height: 100,
+                                    padding: const EdgeInsets.all(8.0),
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    child: Text(
+                                      '100',
+                                      style: TextStyle(
+                                          fontSize: 24,
+                                          fontWeight: FontWeight.bold),
+                                      textAlign: TextAlign.center,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Positioned(
+                        top: 10,
+                        right: 10,
+                        child: PopupMenuButton<String>(
+                          icon: Icon(Icons.filter_list),
+                          onSelected: (String result) {
+                            setState(() {
+                              _selectedFilter = result;
+                              // Implement filter logic
+                              loadData(); // Call loadData() after setting the filter
+                            });
+                          },
+                          itemBuilder: (BuildContext context) =>
+                              <PopupMenuEntry<String>>[
+                            const PopupMenuItem<String>(
+                              value: 'Svi',
+                              child: Text('Svi'),
+                            ),
+                            const PopupMenuItem<String>(
+                              value: 'Padel teren 1',
+                              child: Text('Padel teren 1'),
+                            ),
+                            const PopupMenuItem<String>(
+                              value: 'Padel teren 2',
+                              child: Text('Padel teren 2'),
+                            ),
+                            const PopupMenuItem<String>(
+                              value: 'Padel teren 3',
+                              child: Text('Padel teren 3'),
                             ),
                           ],
                         ),
                       ),
                     ],
-                  ),
-                  const SizedBox(height: 20),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: const Color(0xFFD7D2DC),
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Column(
-                            children: [
-                              Text('Ukupan broj rezervacija',
-                                  style: TextStyle(fontSize: 16)),
-                              Container(
-                                width: 300,
-                                height: 100,
-                                padding: const EdgeInsets.all(8.0),
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                                child: Text(
-                                  '320',
-                                  style: TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold),
-                                  textAlign: TextAlign.center,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                        Expanded(
-                          child: Column(
-                            children: [
-                              Text('Broj iznajmljenih reketa',
-                                  style: TextStyle(fontSize: 16)),
-                              Container(
-                                width: 300,
-                                height: 100,
-                                padding: const EdgeInsets.all(8.0),
-                                decoration: BoxDecoration(
-                                  color: Colors.white,
-                                  borderRadius: BorderRadius.circular(10.0),
-                                ),
-                                child: Text(
-                                  '100',
-                                  style: TextStyle(
-                                      fontSize: 24,
-                                      fontWeight: FontWeight.bold),
-                                  textAlign: TextAlign.center,
-                                ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
                   ),
                   const SizedBox(height: 20),
                   _buildDataListView(),
