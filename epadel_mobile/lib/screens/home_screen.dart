bff45b426f6de95cbc0674b439fa2570263de03c
Amar-Omerika <amar.omerika@edu.fit.ba>
2024-06-06 00:32:45 +0200

    Add filter functionality for tereni

--- epadel_mobile/lib/screens/home_screen.dart
+++ epadel_mobile/lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:epadel_mobile/models/models.dart';
 import 'package:epadel_mobile/providers/providers.dart';
 import 'package:epadel_mobile/widgets/teren_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -57,98 +58,111 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.green[900],
-      body: Stack(
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(top: 80.0),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: TerenInput(
-                            filter: _filter,
-                            onFilterChanged: _onFilterChanged,
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Align(
+                      alignment: AlignmentDirectional.centerEnd,
+                      child: IconButton(
+                        icon: const Icon(
+                          Icons.filter_list,
+                          color: Colors.white,
+                          size: 40,
+                        ),
+                        onPressed: () {
+                          setState(() {
+                            _showFilters = !_showFilters;
+                          });
+                        },
+                      ),
+                    ),
+                  ),        
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: TerenInput(
+                              filter: _filter,
+                              onFilterChanged: _onFilterChanged,
+                            ),
                           ),
-                        ),
-                        IconButton(
-                          icon: const Icon(Icons.filter_list,
-                              color: Colors.white),
-                          onPressed: () {
-                            setState(() {
-                              _showFilters = !_showFilters;
-                            });
-                          },
-                        ),
-                      ],
+                        
+                        ],
+                      ),
                     ),
                   ),
-                ),
-                const SizedBox(height: 5.0),
-                Expanded(
-                  child: _tereni.result.isEmpty &&
-                          (_filter.isNotEmpty || _selectedType != 'Svi')
-                      ? const Center(
-                          child: Text(
-                            'Nema rezultata za vasu pretragu...',
-                            style: TextStyle(color: Colors.white, fontSize: 18),
+                  const SizedBox(height: 5.0),
+                  Expanded(
+                    child: _tereni.result.isEmpty &&
+                            (_filter.isNotEmpty || _selectedType != 'Svi')
+                        ? const Center(
+                            child: Text(
+                              'Nema rezultata za vasu pretragu...',
+                              style:
+                                  TextStyle(color: Colors.white, fontSize: 18),
+                            ),
+                          )
+                        : ListView.builder(
+                            itemCount: _tereni.result.length, // Number of items
+                            itemBuilder: (context, index) {
+                              final teren = _tereni.result[index];
+                              return TerenCard(
+                                teren: teren,
+                              );
+                            },
                           ),
-                        )
-                      : ListView.builder(
-                          itemCount: _tereni.result.length, // Number of items
-                          itemBuilder: (context, index) {
-                            final teren = _tereni.result[index];
-                            return TerenCard(
-                              teren: teren,
-                            );
-                          },
-                        ),
-                ),
-              ],
-            ),
-          ),
-          if (_showFilters)
-            Positioned(
-              top: 120.0,
-              left: 90.0,
-              right: 90.0,
-              child: Container(
-                padding: const EdgeInsets.all(8.0),
-                decoration: BoxDecoration(
-                  color: Colors.green[300],
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Column(
-                  children:
-                      ['Svi', 'Beton', 'Trava', 'Guma'].map((String type) {
-                    return GestureDetector(
-                      onTap: () => _onTypeSelected(type),
-                      child: Container(
-                        margin: const EdgeInsets.symmetric(vertical: 5.0),
-                        decoration: BoxDecoration(
-                          color: type == _selectedType
-                              ? Colors.green[600]
-                              : Colors.white,
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        padding: const EdgeInsets.symmetric(vertical: 10.0),
-                        child: Center(
-                          child: Text(
-                            type,
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                      ),
-                    );
-                  }).toList(),
-                ),
+                  ),
+                ],
               ),
             ),
-        ],
+            if (_showFilters)
+              Positioned(
+                top: 120.0,
+                left: 90.0,
+                right: 90.0,
+                child: Container(
+                  padding: const EdgeInsets.all(8.0),
+                  decoration: BoxDecoration(
+                    color: Colors.green[300],
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: Column(
+                    children:
+                        ['Svi', 'Beton', 'Trava', 'Guma'].map((String type) {
+                      return GestureDetector(
+                        onTap: () => _onTypeSelected(type),
+                        child: Container(
+                          margin: const EdgeInsets.symmetric(vertical: 5.0),
+                          decoration: BoxDecoration(
+                            color: type == _selectedType
+                                ? Colors.green[600]
+                                : Colors.white,
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          padding: const EdgeInsets.symmetric(vertical: 10.0),
+                          child: Center(
+                            child: Text(
+                              type,
+                              style: TextStyle(color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
+          ],
+        ),
       ),
     );
   }
